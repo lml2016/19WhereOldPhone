18dfd26ef512c2d1b8328ff280a41a376f6a34a4
liuminli <liuminli@19where.com>
2016-04-20 15:33:39 +0800

    4

---
+++ 19WhereOldPhone/AspectFitImageView.h
@@ -0,0 +1,13 @@
+//
+//  AspectFitImageView.h
+//  Aiyou
+//
+//  Created by Sniper on 15/10/14.
+//  Copyright © 2015年 skywalker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AspectFitImageView : UIImageView
+
+@end
